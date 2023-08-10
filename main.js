145700b3b8a5e03fa3488d3aab0dd1f848e1118e
Shubham <skg2002007@gmail.com>
2023-08-10 11:47:18 +0530

    js css changed

--- main.js
+++ main.js
@@ -11,7 +11,7 @@ const showMenu = (toggleId, navId) =>{
 }
 showMenu('nav-toggle','nav-menu')
 
-/*==================== REMOVE MENU MOBILE ====================*/
+
 const navLink = document.querySelectorAll('.nav__link')
 
 function linkAction(){
