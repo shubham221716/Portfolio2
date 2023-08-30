68c7c3b666cf545ad742d274bcf6b3cc161323fe
Shubham Kumar Gupta <106598182+shubham221716@users.noreply.github.com>
2023-08-31 00:46:45 +0530

    Update main.js

--- main.js
+++ main.js
@@ -55,5 +55,23 @@ sr.reveal('.home__img, .about__subtitle, .about__text, .skills__img,  .articles'
 sr.reveal('.home__social-icon',{ interval: 200}); 
 sr.reveal('.skills__data, .work__img, .contact__input',{interval: 200}); 
 
+ // Get the button
+        let mybutton = document.getElementById("myBtn");
 
+        // When the user scrolls down 20px from the top of the document, show the button
+        window.onscroll = function () { scrollFunction() };
+
+        function scrollFunction() {
+            if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+                mybutton.style.display = "block";
+            } else {
+                mybutton.style.display = "none";
+            }
+        }
+
+        // When the user clicks on the button, scroll to the top of the document
+        function topFunction() {
+            document.body.scrollTop = 0;
+            document.documentElement.scrollTop = 0;
+        }
 
